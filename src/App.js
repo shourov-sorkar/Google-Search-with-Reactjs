154261aee9f5455c0dbdbb7869180969bd11cc31
shourov-sorkar <18103142@iubat.edu>
2021-04-02 13:49:13 +0600

    :heavy_check_mark: Router Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Index from "./components/HomeSceen/Index";
+import SearchScreen from "./components/Search/SearchScreen";
 function App() {
   return (
-    <div className="App">
-      <Index />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path={"/"} component={() => <Index />} />
+          <Route exact path={"/search"} component={() => <SearchScreen />} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Search/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchScreen() {
+    return (
+        <div>
+            Welcome to the search page 
+        </div>
+    )
+}
+
+export default SearchScreen;
